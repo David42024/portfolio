3f1f96877e189cb48d85f37d28bf8c6b885d05b8
David42024 <daln486279513@gmail.com>
2026-02-03 11:38:23 -0500

    cambios front

--- apps/frontend/src/services/api.ts
+++ apps/frontend/src/services/api.ts
@@ -1,4 +1,5 @@
-const API_URL =  "http://localhost:3001/api/v1";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api/v1";
+
 
 interface ApiResponse<T> {
   success: boolean;
