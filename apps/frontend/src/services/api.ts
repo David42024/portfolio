b4ce08ecc756f2fb93931a48ebf3903aa6bd7895
David42024 <daln486279513@gmail.com>
2026-02-04 01:26:38 -0500

    econnrefused

--- apps/frontend/src/services/api.ts
+++ apps/frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const APP_URL = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3001";
+const APP_URL = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:4000";
 const API_URL = `${APP_URL}/api/v1`;
 
 
