a28675e3dcce176365b331b7be2980d23360faed
David42024 <daln486279513@gmail.com>
2026-02-06 00:32:27 -0500

    ssl

--- apps/backend/src/config/db.ts
+++ apps/backend/src/config/db.ts
@@ -3,7 +3,11 @@ import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
-  ssl: false, // 👈 fuerza a NO usar certs locales
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({
+  adapter,
+});
