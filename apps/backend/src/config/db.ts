4023a44e836b53640d31daf3870aa6407513c4d8
David42024 <daln486279513@gmail.com>
2026-02-05 23:51:25 -0500

    hola

--- apps/backend/src/config/db.ts
+++ apps/backend/src/config/db.ts
@@ -1,5 +1,11 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../../prisma/generated/client'
+import fs from 'fs'
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL, sslMode: 'require' })
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    ca: fs.readFileSync("./certs/ca.crt")
+  }
+})
 export const prisma = new PrismaClient({ adapter })
